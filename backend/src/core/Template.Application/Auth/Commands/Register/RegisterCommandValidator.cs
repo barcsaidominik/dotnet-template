e79fe3fd8fcbd84c6c8bd24cb2cc054db263b9ae
Barcsai Dominik Zoltán <barcsai.dominik@gmail.com>
2026-04-22 17:29:35 +0200

    Validator update.

--- backend/src/core/Template.Application/Auth/Commands/Register/RegisterCommandValidator.cs
+++ backend/src/core/Template.Application/Auth/Commands/Register/RegisterCommandValidator.cs
@@ -6,12 +6,5 @@ public sealed class RegisterCommandValidator : AbstractValidator<RegisterCommand
 {
     public RegisterCommandValidator()
     {
-        RuleFor(x => x.Email).NotEmpty().EmailAddress();
-        RuleFor(x => x.Password)
-            .NotEmpty()
-            .MinimumLength(12)
-            .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter")
-            .Matches("[0-9]").WithMessage("Password must contain at least one digit")
-            .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character");
     }
 }
