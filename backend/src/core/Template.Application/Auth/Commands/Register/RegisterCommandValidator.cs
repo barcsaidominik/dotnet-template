6a87713d4dc2a40db158876cca19e0514f004dec
Barcsai Dominik Zoltán <barcsai.dominik@gmail.com>
2026-04-25 13:23:52 +0200

    Unnecessary validator remove.

--- backend/src/core/Template.Application/Auth/Commands/Register/RegisterCommandValidator.cs
+++ backend/src/core/Template.Application/Auth/Commands/Register/RegisterCommandValidator.cs
@@ -6,12 +6,6 @@ public sealed class RegisterCommandValidator : AbstractValidator<RegisterCommand
 {
     public RegisterCommandValidator()
     {
-        RuleFor(x => x.Email).NotEmpty().EmailAddress();
-        RuleFor(x => x.Password)
-            .NotEmpty()
-            .MinimumLength(12)
-            .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter")
-            .Matches("[0-9]").WithMessage("Password must contain at least one digit")
-            .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character");
+        // Nem kell validáció, a frontend végzi.
     }
 }
