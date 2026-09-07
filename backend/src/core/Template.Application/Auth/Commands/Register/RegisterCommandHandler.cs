8c36e8809adfaa31dbfac20de0d9fa60b93fa5b4
Barcsai Dominik Zoltán <34102660+barcsaidominik@users.noreply.github.com>
2026-09-07 14:56:55 +0200

    fix: use ephemeral data protection in tests, invalidate user cache on register (#93)

--- backend/src/core/Template.Application/Auth/Commands/Register/RegisterCommandHandler.cs
+++ backend/src/core/Template.Application/Auth/Commands/Register/RegisterCommandHandler.cs
@@ -1,15 +1,25 @@
 using ErrorOr;
 using Mediator;
+using Microsoft.Extensions.Caching.Memory;
+using Template.Application.Common;
 using Template.Application.Common.Interfaces;
 
 namespace Template.Application.Auth.Commands.Register;
 
-public sealed class RegisterCommandHandler(IAuthService authService) : IRequestHandler<RegisterCommand, ErrorOr<Success>>
+public sealed class RegisterCommandHandler(IAuthService authService, IMemoryCache cache) : IRequestHandler<RegisterCommand, ErrorOr<Success>>
 {
     private readonly IAuthService _authService = authService;
+    private readonly IMemoryCache _cache = cache;
 
     public async ValueTask<ErrorOr<Success>> Handle(RegisterCommand request, CancellationToken ct)
     {
-        return await _authService.RegisterAsync(request.Email, request.Password, ct);
+        var result = await _authService.RegisterAsync(request.Email, request.Password, ct);
+
+        if (!result.IsError)
+        {
+            _cache.Remove(CacheKeys.ALL_USERS);
+        }
+
+        return result;
     }
 }
